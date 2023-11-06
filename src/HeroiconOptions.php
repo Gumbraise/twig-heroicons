d95c353c1b9d6274977bc01eb68af9b04d99cf73
Gumbraise <vikiclashonyt@gmail.com>
2023-11-06 22:32:08 +0100

    ⚡️ Added all tags allowed on <svg>

--- src/HeroiconOptions.php
+++ src/HeroiconOptions.php
@@ -6,24 +6,24 @@ class HeroiconOptions
 {
     public static function applyOptions(string $svg, array $options): string
     {
-        if (isset($options['class'])) {
-            $svg = str_replace('<svg', sprintf('<svg class="%s"', $options['class']), $svg);
-        }
+        foreach ($options as $key => $value) {
+            if (isset($options['stroke-width'])) {
+                $svg = preg_replace('/\s(stroke-width)="[^"]*"/', ' stroke-width="' . $options['stroke-width'] . '"', $svg);
+            }
 
-        if (isset($options['stroke-width'])) {
-            $svg = preg_replace('/\s(stroke-width)="[^"]*"/', ' stroke-width="' . $options['stroke-width'] . '"', $svg);
-        }
+            if (isset($options['stroke'])) {
+                $svg = preg_replace('/\s(stroke)="[^"]*"/', ' stroke="' . $options['stroke'] . '"', $svg);
+            } else {
+                $svg = preg_replace('/\s(stroke)="[^"]*"/', '', $svg);
+            }
 
-        if (isset($options['stroke'])) {
-            $svg = preg_replace('/\s(stroke)="[^"]*"/', ' stroke="' . $options['stroke'] . '"', $svg);
-        } else {
-            $svg = preg_replace('/\s(stroke)="[^"]*"/', '', $svg);
-        }
+            if (isset($options['fill'])) {
+                $svg = preg_replace('/\s(fill)="[^"]*"/', ' fill="' . $options['fill'] . '"', $svg);
+            } else {
+                $svg = preg_replace('/\s(fill)="[^"]*"/', '', $svg);
+            }
 
-        if (isset($options['fill'])) {
-            $svg = preg_replace('/\s(fill)="[^"]*"/', ' fill="' . $options['fill'] . '"', $svg);
-        } else {
-            $svg = preg_replace('/\s(fill)="[^"]*"/', '', $svg);
+            $svg = str_replace('<svg', sprintf('<svg %s="%s"', $key, $value), $svg);
         }
 
         return $svg;
